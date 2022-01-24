1496af542f9d6e123f389bb8ee85ebd1662959d7
sinmugyeol <tlsanruf123@gmail.com>
2022-01-25 00:57:20 +0900

    [ADD] utils

---
+++ YOLO/utils.py
@@ -0,0 +1,63 @@
+import os
+import torch
+import torch.nn as nn
+import numpy as np
+
+def set_requires_grad(nets, requires_grad=False):
+    if not isinstance(nets, list):
+        nets = [nets]
+    for net in nets:
+        if net is not None:
+            for param in net.parameters():
+                param.requires_grad = requires_grad
+
+def save(ckpt_dir, netG, netD, optimG, optimD, epoch):
+    if not os.path.exists(ckpt_dir):
+        os.makedirs(ckpt_dir)
+
+    torch.save({'netG': netG.state_dict(), 'netD': netD.state_dict, 'optimG': optimG.state_dict(),
+                'optimD': optimD.state_dict()}, '%s/model_epoch%d.pth' % (ckpt_dir, epoch))
+
+def load(ckpt_dir, netG, netD, optimG, optimD):
+    if not os.path.exists(ckpt_dir):
+        epoch = 0
+        return netG, netD, optimG, optimD, epoch
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    ckpt_lst = os.listdir(ckpt_dir)
+    ckpt_lst = [f for f in ckpt_lst if f.endswith('pth')]
+    ckpt_lst.sort(key=lambda f: int(''.join(filter(str.isdigit(), f))))
+
+    dict_model = torch.load('%s/%s' % (ckpt_dir, ckpt_lst[-1]), map_location=device)
+
+    netG.load_state_dict(dict_model['netG'])
+    netD.load_state_dict(dict_model['netD'])
+    optimG.load_state_dict(dict_model['optimG'])
+    optimD.load_state_dict(dict_model['optimD'])
+    epoch = int(ckpt_lst[-1].split('epoch')[1].split('.pth')[0])
+
+    return netG, netD, optimG, optimD, epoch
+
+# 가중치 초기화
+def init_weights(net, init_type='normal', init_gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            if init_type == 'normal':
+                nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                nn.init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                nn.init.constant_(m.bias.data, 0.0)
+        elif classname.find('BatchNorm2d') != -1:
+            nn.init.normal_(m.weight.data, 1.0, init_gain)
+            nn.init.constant_(m.bias.data, 0.0)
+
+    net.apply(init_func)
