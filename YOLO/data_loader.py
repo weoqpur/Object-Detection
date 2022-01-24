4bef747f7a534989326cc34bdf6014d6f226a92a
sinmugyeol <tlsanruf123@gmail.com>
2022-01-25 00:48:13 +0900

    [UPDATE] add __getitem__

--- YOLO/data_loader.py
+++ YOLO/data_loader.py
@@ -7,23 +7,53 @@ import torch
 import torch.nn as nn
 from torch.utils.data.dataset import Dataset
 from PIL import Image
-import tqdm
+from skimage.transform import resize
 
 class_names = ['apple', 'banana', 'orange']
 
 class Dataset(Dataset):
-    def __init__(self, file_dir):
+    def __init__(self, file_dir, transform=None):
         """
         :param file_dir: dataset 경로
+        :param transform: transform 여부
         """
 
         super(Dataset, self).__init__()
         self.img_files = [os.path.join(file_dir, img_file) for img_file in sorted(os.listdir(file_dir))
                           if img_file[-4:] == '.jpg']
         self.annot_files = [img_file[:-4] + '.xml' for img_file in self.img_files]
+        self.transform = transform
+        self.to_tensor = ToTensor()
+
 
     def __len__(self):
         return len(self.img_files)
+
+    def __getitem__(self, index):
+        img = self.img_files[index]
+        target = extract_annotation_file(self.annot_files[index])
+        target = convert_bboxes_to_tensor(*target)
+
+        if img.dtype == np.uint8:
+            img = img / 255.0
+
+        if self.transform:
+            img = self.transfrom(img)
+
+        img = self.to_tensor(img)
+
+        data = {'image': img, 'target': target}
+
+        return data
+
+
+
+
+
+
+
+
+
 
 
 def convert_to_xywh(bboxes):
@@ -100,3 +130,57 @@ def convert_bboxes_to_tensor(bboxes, classes, img_width, img_height, grid_size=7
         target[i, j, 5 + classes[idx]] = 1.
 
     return target
+
+class ToTensor(object):
+    def __call__(self, data):
+        for key, value in data.items():
+            value = value.transpose((2, 0, 1)).astype(np.float32)
+            data[key] = torch.from_numpy(value)
+
+        return data
+
+
+class Normalization(object):
+    def __init__(self, mean=0.5, std=0.5):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, data):
+        for key, value in data.items():
+            data[key] = (value - self.mean) / self.std
+
+        return data
+
+
+class RandomCrop(object):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def __call__(self, data):
+
+        key = list(data.keys())[0]
+
+        h, w = data[key].shape[:2]
+        new_h, new_w = self.shape
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        id_y = np.arange(top, top + new_h, 1)[:, np.newaxis]
+        id_x = np.arange(left, left + new_w, 1)
+
+        for key, value in data.items():
+            data[key] = value[id_y, id_x]
+
+        return data
+
+
+class Resize(object):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def __call__(self, data):
+        for key, value in data.items():
+            data[key] = resize(value, output_shape=(self.shape[0], self.shape[1], self.shape[2]))
+
+        return data
