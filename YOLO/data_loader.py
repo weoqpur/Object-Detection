096f9438e930be51145cd0b9edb072f23aee3423
sinmugyeol <tlsanruf123@gmail.com>
2022-01-26 02:02:58 +0900

    [UPDATE] __getitem__

--- YOLO/data_loader.py
+++ YOLO/data_loader.py
@@ -19,30 +19,52 @@ class Dataset(Dataset):
         """
 
         super(Dataset, self).__init__()
-        self.img_files = [os.path.join(file_dir, img_file) for img_file in sorted(os.listdir(file_dir))
-                          if img_file[-4:] == '.jpg']
-        self.annot_files = [img_file[:-4] + '.xml' for img_file in self.img_files]
+        img_files = [img_file for img_file in sorted(os.listdir(file_dir)) if img_file[-4:] == '.jpg']
+        annot_files = [img_file[:-4] + '.xml' for img_file in self.img_files]
+        df = pd.concat([img_files, annot_files], axis=1)
+        self.df = pd.DataFrame(df)
+        self.file_dir = file_dir
         self.transform = transform
         self.to_tensor = ToTensor()
 
 
     def __len__(self):
-        return len(self.img_files)
+        return len(self.df)
 
     def __getitem__(self, index):
-        img = self.img_files[index]
-        target = extract_annotation_file(self.annot_files[index])
-        target = convert_bboxes_to_tensor(*target)
+        label_path = os.path.join(self.file_dir, self.df.iloc[index, 1])
+        boxes = torch.tensor(extract_annotation_file(label_path))
 
-        if img.dtype == np.uint8:
-            img = img / 255.0
+        image_path = os.path.join(self.file_dir, self.df.iloc[index, 0])
+        image = Image.open(image_path)
+        image = image.convert("RGB")
 
         if self.transform:
-            img = self.transfrom(img)
+            image = self.transfrom(image)
+        image = self.to_tensor(image)
 
-        img = self.to_tensor(img)
+        label_matrix = torch.zeros((7, 7, 13))
+        for box in boxes:
+            class_label, x, y, width, height = box.tolist()
+            class_label = int(class_label)
 
-        data = {'image': img, 'target': target}
+            i, j = int(7 * y), int(7 * x)
+            x_cell, y_cell = 7 * x - j, 7 * y - i
+
+            width_cell, height_cell = width * 7, height * 7
+
+            print(i, j)
+            if label_matrix[i, j, 3] == 0:
+                label_matrix[i, j, 3] = 1
+
+                box_coordinates = torch.tensor([x_cell, y_cell, width_cell, height_cell])
+
+                label_matrix[i, j, 4:8] = box_coordinates
+
+                label_matrix[i, j, class_label] = 1
+
+
+        data = {'image': image, 'label': label_matrix}
 
         return data
 
@@ -50,38 +72,14 @@ class Dataset(Dataset):
 
 
 
-
-
-
-
-
-
-def convert_to_xywh(bboxes):
-    """
-    :param bboxes: xmin, ymin, xmax, ymax를 포함한 list
-    :return: x_center, y_center, box_width, box_height
-    """
-
-    boxes = list()
-    for box in bboxes:
-        xmin, ymin, xmax, ymax = box
-
-        box_width = xmax - xmin
-        box_height = ymax - ymin
-
-        x_center = xmin + (box_width // 2)
-        y_center = ymin + (box_height // 2)
-
-        boxes.append((x_center, y_center, box_width, box_height))
-
-    return boxes
-
 def extract_annotation_file(file_name):
     tree = ElementTree.parse(file_name)
 
     root = tree.getroot()
     boxes = list()
-    classes = list()
+
+    img = Image.open(file_name[:-4] + '.jpg')
+    width, height = img.size[0], img.size[1]
 
     for box in root.findall('.//object'):
         cls = class_names.index(box.find('name').text)
@@ -89,24 +87,19 @@ def extract_annotation_file(file_name):
         ymin = int(box.find('bndbox/ymin').text)
         xmax = int(box.find('bndbox/xmax').text)
         ymax = int(box.find('bndbox/ymax').text)
-        coors = (xmin, ymin, xmax, ymax)
-        boxes.append(coors)
-        classes.append(cls)
 
-    boxes = convert_to_xywh(boxes)
+        x_center = ((xmax - xmin) / 2) / width
+        y_center = ((ymax - ymin) / 2) / height
+        box_width = (xmax - xmin) / width
+        box_height = (ymax - ymin) / height
 
-    width = int(root.find('.//size/width').text)
-    height = int(root.find('.//size/height').text)
+        boxes.append([cls, x_center, y_center, box_width, box_height])
 
-    if width == 0 or height == 0:
-        img = Image.open(file_name[:-4] + '.jpg')
-        width, height = img.size[0], img.size[1]
-
-    return boxes, classes, width, height
+    return boxes
 
 def convert_bboxes_to_tensor(bboxes, classes, img_width, img_height, grid_size=7):
     num_classes = len(class_names)
-    target = np.zeros(shape=(grid_size, grid_size, 5 + num_classes), dtype=np.float32)
+    target = torch.zeros(shape=(grid_size, grid_size, 5 + num_classes), dtype=np.float32)
 
     for idx, bbox in enumerate(bboxes):
         x_center, y_center, width, height = bbox
@@ -124,7 +117,7 @@ def convert_bboxes_to_tensor(bboxes, classes, img_width, img_height, grid_size=7
         w_norm, h_norm = width / img_width, height / img_height
 
         # x, y, w, h 설정
-        target[i, j, :4] += (x, y, w_norm, h_norm)
+        target[i, j, :4] += torch.tensor(x, y, w_norm, h_norm)
         target[i, j, :4] = 1.
         # class dist 설정
         target[i, j, 5 + classes[idx]] = 1.
