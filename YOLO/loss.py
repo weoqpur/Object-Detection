2badea283a403c92b0611b7fe6695e88c81a5600
sinmugyeol <tlsanruf123@gmail.com>
2022-01-27 00:05:37 +0900

    [UPDATE] YoloLoss implementation

--- YOLO/loss.py
+++ YOLO/loss.py
@@ -42,3 +42,102 @@ def intersection_over_union(boxes_preds, boxes_labels, box_format='midpoint'):
     box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_x1))
 
     return intersection / (box1_area + box2_area - intersection + 1e-6)
+
+class YoloLoss(nn.Module):
+    def __init__(self, S=7, B=2, C=3):
+        """
+        :param S: 이미지 split size (in paper 7)
+        :param B: box의 수 (in paper 2)
+        :param C: class의 수 (in paper 20, in dataset 3)
+        """
+        super(YoloLoss, self).__init__()
+        self.mse = nn.MSELoss(reduction='sum')
+        self.S = S
+        self.B = B
+        self.C = C
+
+        self.lambda_noobj = 0.5
+        self.lambda_coord = 5.
+
+    def forward(self, predictions, target):
+        # prediction shape == (batch, S*S(C+B*5))
+        predictions = predictions.reshape(-1, self.S, self.S, self.C + self.B * 5)
+
+        iou_b1 = intersection_over_union(predictions[..., self.C + 1:self.C + 5], target[..., self.C + 1:self.C + 5])
+        iou_b2 = intersection_over_union(predictions[..., self.C + 6:self.C + 10], target[..., self.C + 1:self.C + 5])
+        ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim=0)
+
+        # 두 개의 IoU중 제일 좋은 prediction 선택
+        iou_maxes, bestbox = torch.max(ious, dim=0)
+        exists_box = target[..., self.C].unsqueeze(3)
+
+        # ===============
+        # box coordinates
+        # ===============
+
+        # 개체가 없는 상자는 0으로 설정, IoU 두 개중 하나만 선택
+        box_predictions = exists_box * (
+            (
+                bestbox * predictions[..., self.C + 6:self.C + 10]
+                + (1 - bestbox) * predictions[..., self.C + 1:self.C + 5]
+            )
+        )
+
+        box_targets = exists_box * target[..., self.C + 1:self.C + 5]
+
+        # 상자의 너비와 높이를 제곱
+        box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) * torch.sqrt(
+            torch.abs(box_predictions[..., 2:4] + 1e-6)
+        )
+        box_targets[..., 2:4] = torch.sqrt(box_targets[..., 2:4])
+
+        box_loss = self.mse(
+            torch.flatten(box_predictions, end_dim=-2),
+            torch.flatten(box_targets, end_dim=-2)
+        )
+
+        # ===========
+        # object loss
+        # ===========
+
+        # pred_box는 가장 높은 IoU를 할당
+        pred_box = (
+            bestbox * predictions[..., self.C + 5:self.C + 6] + (1 - bestbox) * predictions[..., self.C:self.C + 1]
+        )
+
+        object_loss = self.mse(
+            torch.flatten(exists_box * pred_box),
+            torch.flatten(exists_box * target[..., self.C:self.C + 1])
+        )
+
+        # ==============
+        # no object loss
+        # ==============
+
+        no_object_loss = self.mse(
+            torch.flatten((1 - exists_box) * predictions[..., self.C:self.C + 1], start_dim=1),
+            torch.flatten((1 - exists_box) * target[..., self.C:self.C + 1], start_dim=1)
+        )
+
+        no_object_loss += self.mse(
+            torch.flatten((1 - exists_box) * predictions[..., self.C + 5:self.C + 6], start_dim=1),
+            torch.flatten((1 - exists_box) * target[..., self.C:self.C + 1], start_dim=1)
+        )
+
+        # ==========
+        # class loss
+        # ==========
+
+        class_loss = self.mse(
+            torch.flatten(exists_box * predictions[..., :self.C], end_dim=-2),
+            torch.flatten(exists_box * target[..., :self.C], end_dim=-2)
+        )
+
+        loss = (
+            self.lambda_coord * box_loss # 논문 loss 첫번째줄
+            + object_loss # 두번째줄
+            + self.lambda_noobj * no_object_loss # 세번째줄
+            + class_loss # 네번째줄
+        )
+
+        return loss
